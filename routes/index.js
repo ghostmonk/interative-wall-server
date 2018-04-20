312b24aa8bccaed9e4bca33074b42792bd9cf19b
Matan Kushner <hello@matchai.me>
2018-04-20 00:33:38 -0400

    WEBSOCKETS WORK

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
+const WebSocket = require('ws');
 const express = require('express');
 const router = express.Router();
 const quickstart = require('./quickstart');
-const wss = require('../bin/www').wss;
 
 router.get('/*', function(req, res, next) {
+  const wss = require('../bin/www').wss;
   const secrets = fs.readFileSync('test/client_secret.json', 'utf8');
   return quickstart.authorize(JSON.parse(secrets))
     .then((result) => {
       wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(result);
+        if (client.readyState === 1) {
+          client.send(JSON.stringify(result));
         }
       });
       return res.send("success");
