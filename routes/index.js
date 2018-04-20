49fd70bc6d15fc391ea051e10be8eb4a031c2c13
nhillier <nhillier@premiumbeat.com>
2018-04-19 21:34:37 -0400

    STUFFFF

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const quickstart = require('./quickstart');
 
 router.get('/', function(req, res, next) {
-  fs.readFile('routes/client_secret.json', (err, content) => {
-    if (err) {
-      return res.send('Error loading client secret file:', err);
-    }
-    const result = quickstart.authorize(JSON.parse(content));
-    res.send(result);
-  });
+  const secrets = fs.readFileSync('routes/client_secret.json', 'utf8');
+  return quickstart.authorize(JSON.parse(secrets))
+    .then((result) => {
+      return res.json(result);
+    });
 });
 
 module.exports = router;
