5475e763180c40bd2b667cb32287079887f7a259
nhillier <nhillier@premiumbeat.com>
2018-04-19 22:48:29 -0400

    I put the lime in the coconut

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const quickstart = require('./quickstart');
+const websocket = require('../bin/www').wss;
 
 router.get('/*', function(req, res, next) {
   const secrets = fs.readFileSync('routes/client_secret.json', 'utf8');
   return quickstart.authorize(JSON.parse(secrets))
     .then((result) => {
-      return res.json(result);
+      websocket.send(result);
+      return res.send("success");
     });
 });
 
