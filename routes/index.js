747fd79a0680f1d05a6f822362c8b1938ff297a2
Matan Kushner <hello@matchai.me>
2018-04-19 23:08:18 -0400

    Fix websocket event

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const quickstart = require('./quickstart');
-const websocket = require('../bin/www').wss;
+const wss = require('../bin/www').wss;
 
 router.get('/*', function(req, res, next) {
   const secrets = fs.readFileSync('routes/client_secret.json', 'utf8');
   return quickstart.authorize(JSON.parse(secrets))
     .then((result) => {
-      websocket.send(result);
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(result);
+        }
+      });
       return res.send("success");
     });
 });
